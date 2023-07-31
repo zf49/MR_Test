e711bf386a40038cadd3266f304f59fe5c2f17a4
zf49 <zwan686@aucklanduni.ac.nz>
2023-07-31 22:24:46 +1200

    add cart button:

--- src/view/AddToCart.js
+++ src/view/AddToCart.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import Button from '@mui/material/Button'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
 
 export default function AddToCart(props) {
 
+  const [clickedButton, setClickedButton] = useState(null);
+
   return (
-    <div>
-        {console.log(props.ProductSiz)}
-    </div>
+   
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <div>
+          {props.Size.map((item,index) => {
+            return (
+              <Button
+                key={index}
+                sx={{
+                  width: "50px",
+                  height: "50px",
+                  marginLeft: "0.5em",
+                  border: clickedButton === index ? "2px solid #222222" : "1px solid #CCCCCC",
+                  borderRadius: "0",
+                  color: "#888888",
+                  marginBottom: '1em'
+                }}
+                onClick={() => setClickedButton(index)}
+              >
+                {item.label}
+              </Button>
+            )
+          })}
+        </div>
+        <div>
+          <Button sx={{color:"#222222",border:"2px solid black", borderRadius: "0",marginLeft:'0.5em'}}>
+            ADD TO CART
+          </Button>
+        </div>
+      </Box>
+
   )
 }
--- src/view/GoodsInfo.js
+++ src/view/GoodsInfo.js
@@ -2,26 +2,27 @@ import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-
 import axios from 'axios'
 import AddToCart from './AddToCart';
-
-
+import { Button } from '@mui/material';
 
 export default function GoodsInfo() {
+
+  const [goodsDetail, setGoodsDetail] = useState({ sizeOptions: [] });
+
 
   useEffect(() => {
     axios.get('https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product').then((res)=>{
       setGoodsDetail(res.data)
+      console.log(res.data)
     })
 
   },[])
-  
-  const [goodsDetail, setGoodsDetail] = useState({})
 
-
+ 
   return (
-      <Grid container spacing={2} alignItems="center" justifyContent="center" sx={{marginTop:'1em'}}>
+
+      <Grid container spacing={2} alignItems="flex-star" justifyContent="center" sx={{marginTop:'1em'}}>
         <Grid item xs={12} sm={6}>
           <Box
             component="img"
@@ -31,23 +32,25 @@ export default function GoodsInfo() {
         </Grid>
         <Grid item xs={12} sm={6}>
 
-          <Typography variant="h6" align="left" gutterBottom sx={{ color: '#222222' }}>
+          <Typography variant="h6" align="left" gutterBottom sx={{ color: '#222222', marginBottom: '1em' }}>
             {goodsDetail.title}
           </Typography>
 
-          <Typography variant="body1" align="left" gutterBottom sx={{ color: '#222222',borderTop: '0.5px solid #888888', borderBottom: '0.5px solid #888888' }}>
+          <Typography variant="body1" align="left" gutterBottom sx={{ color: '#222222',borderTop: '0.5px solid #CCCCCC', borderBottom: '0.5px solid #CCCCCC' ,                    marginBottom: '1em'}}>
             {"$"+goodsDetail.price}
           </Typography>
 
-          <Typography variant="body2" align="left" sx={{color:'#888888'}}>
+          <Typography variant="body2" align="left" sx={{color:'#888888',                    marginBottom: '1em'}}>
             {goodsDetail.description}
           </Typography>
 
-          <Typography variant="body2" align="left" sx={{color:'#888888'}}>
-            SIZE<span style={{color: 'red'}}>*</span>
+          <Typography variant="body2" align="left" sx={{color:"#888888",                    marginBottom: '1em'}}>
+            SIZE<span style={{color: "#C90000"}}>*</span>
           </Typography>
-
-          <AddToCart  ProductSiz={goodsDetail.sizeOptions}/>
+          {console.log(goodsDetail)}
+          
+          <AddToCart Size={goodsDetail.sizeOptions}/>
+         
 
         </Grid>
       </Grid>
